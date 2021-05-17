28327ee653c65da758935a72e853654483094d5a
Maarten van Vliet <maartenvanvliet@gmail.com>
2021-05-17 14:29:57 +0200

    Fix formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [step: 1, step: 2, step: 3, params: 1]
+  locals_without_parens: [step: 1, step: 2, params: 1]
 ]
